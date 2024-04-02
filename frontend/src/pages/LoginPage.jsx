b381cb3b461e997527058493216e956b394333a2
farhan ansari <farhanraza2239@gmail.com>
2024-04-02 14:19:18 +0530

    complete login page UI and functionality and add also setup redux

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles/login.scss"
+// import { setLogin } from "../redux/state";
+import { setLogin } from "../features/authSlice";
+import { useDispatch } from "react-redux"
+import { useNavigate,Link } from "react-router-dom"
 
 function LoginPage() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch()
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch ("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      })
+
+      /* Get data after fetching */
+      const loggedIn = await response.json()
+
+      if (loggedIn) {
+        dispatch (
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token
+          })
+        )
+        navigate("/")
+      }
+
+    } catch (err) {
+      console.log("Login failed", err.message)
+    }
+  }
+
+
+
+
+
   return (
-    <div>LoginPage</div>
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+        <Link to={'/register'} >Don't have an account? Sign In Here</Link>
+      </div>
+    </div>
   )
 }
 
