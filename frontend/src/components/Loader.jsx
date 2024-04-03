1b9a1c039975f33b71a500d3fea7657f993e9030
farhan ansari <farhanraza2239@gmail.com>
2024-04-03 14:58:33 +0530

    create loader component

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Loader.scss'
+
+function Loader() {
+  return (
+    <div className='loader'>
+    <div className='loader-inner'></div>
+  </div>
+  )
+}
+
+export default Loader
